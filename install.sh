9deef6d075b1f57d0814e60ec996720d5d9df1cb
jsupratman13 <supratmanjoshua@gmail.com>
2024-05-13 10:34:24 +0900

    update install method

--- install.sh
+++ install.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
+# cuda 11
+sudo apt install libopenblas-dev
 pipenv install
 pipenv install git+https://github.com/NVIDIA/MinkowskiEngine
+
+# cuda 12
+pipenv shell
+pip install ninja open3d numpy easydict
+pip install torch==2.2.2
+pip install -U git+https://github.com/pccws/MinkowskiEngine -v --no-deps
